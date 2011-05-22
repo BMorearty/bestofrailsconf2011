507108511891f655120f5df6d01c7249d2ab220f
Brian Morearty <!security 2&1 /dev/null find-generic-password -gs git.email    | ruby -e 'print $1 if STDIN.gets =~ /^password: \"(.*)\"$/>
2011-05-22 12:05:33 -0700

    Refactor, fix, add tests.

--- app/models/scrape.rb
+++ app/models/scrape.rb
@@ -1,2 +1,15 @@
 class Scrape < ActiveRecord::Base
+  default_scope order('id')
+
+  def self.update_last_scrape_time
+    if scrape = Scrape.first
+      # Force a save.  If we call updated_at_will_change!, updated_at *won't* change (because
+      # it overrides the attribute with its own value).  So we call created_at_will_change!
+      # to make updated_at change.  Weird, I know.
+      scrape.created_at_will_change!
+      scrape.save!
+    else
+      Scrape.create!
+    end
+  end
 end
--- test/unit/scrape_test.rb
+++ test/unit/scrape_test.rb
@@ -1,8 +1,23 @@
 require 'test_helper'
 
 class ScrapeTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "first call of update_last_scrape_time creates a Scrape instance" do
+    Scrape.delete_all
+    assert_equal 0, Scrape.count
+    Scrape.update_last_scrape_time
+    assert_equal 1, Scrape.count
+  end
+
+  test "subsequent call of update_last_scrape_time does not create a Scrape instance" do
+    assert_equal 1, Scrape.count
+    Scrape.update_last_scrape_time
+    assert_equal 1, Scrape.count
+  end
+
+  test "calling update_last_scrape_time updates the last scrape time" do
+    attrs = Scrape.first.attributes
+    Scrape.update_last_scrape_time
+    assert_equal     attrs["created_at"], Scrape.first.created_at
+    assert_not_equal attrs["updated_at"], Scrape.first.updated_at
   end
 end
